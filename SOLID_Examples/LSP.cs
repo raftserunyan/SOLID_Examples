9f746b809e782b0fb20a5580590f92ebb599ae30
Rafik Tserunyan <rafik.tserunyan@dataart.com>
2021-07-16 18:49:31 +0400

    Add project files.

---
+++ SOLID_Examples/LSP.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SOLID_Examples
+{
+	//If S is a subtype of T, then objects of type T may be replaced with objects of type S 
+
+	class GearBox
+	{
+		public int CountOfGears { get; set; }
+		public int CurrentGear { get; set; }
+
+		private readonly List<int> _gears;
+
+		public GearBox(int countOfGears)
+		{
+			this.CountOfGears = countOfGears;
+
+			for (int i = -1; i <= CountOfGears; i++)
+			{
+				_gears.Add(i);
+			}
+
+			CurrentGear = _gears[1];
+		}
+
+		public void UpShift()
+		{
+			if (this.CurrentGear < this.CountOfGears)
+				this.CurrentGear++;
+		}
+
+		public void DownShift()
+		{
+			if (this.CurrentGear > 0)
+				this.CurrentGear--;
+		}
+
+	}
+	abstract class Car
+	{
+		protected readonly GearBox GearBox;
+
+		public Car(int countOfGears)
+		{
+			this.GearBox = new GearBox(countOfGears);
+		}
+
+		public abstract void Drive();
+		public abstract void UpShift();
+		public abstract void DownShift();
+	}
+
+	class Toyota : Car
+	{
+		public Toyota(int countOfGears) : base(countOfGears)
+		{
+
+		}
+
+		public override void Drive()
+		{
+			if (this.GearBox.CurrentGear != 0)
+			{
+				//Move()
+			}
+		}
+		public override void UpShift()
+		{
+			this.GearBox.UpShift();
+		}
+		public override void DownShift()
+		{
+			this.GearBox.DownShift();
+		}
+	}
+
+	class Tesla : Car
+	{
+		public Tesla() : base(1)
+		{
+
+		}
+
+		public override void Drive()
+		{
+			if (this.GearBox.CurrentGear == 1)
+			{
+				//Move()
+			}
+		}
+		public override void UpShift()
+		{
+			//This breaks the Liskov Substitution principle
+			throw new Exception("A tesla has only one gear, it cannot upshift.");
+		}
+		public override void DownShift()
+		{
+			//This breaks the Liskov Substitution principle
+			throw new Exception("A tesla has only one gear, it cannot upshift.");
+		}
+	}
+}
---
+++ SOLID_Examples/SRP.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace SOLID_Examples
+{
+	#region Bad Code
+	class Bakery
+	{
+		public void RegisterOrder(Customer customer, BakeryProduct product, int quantity)
+		{
+			//Some logic for saving the order
+			SendEmail(customer, "Your order was registered successfully");
+		}
+
+		public void Bake(Order order)
+		{
+			//Some baking logic
+			SendEmail(order.Customer, "Your food is ready");
+		}
+
+		private void SendEmail(Customer customer, string message)
+		{
+			//Sending an email with this context "Your order is ready"
+		}
+
+	}
+	public class Order
+	{
+		public Customer Customer { get; set; }
+	}
+	public class Customer
+	{
+
+	}
+	public enum BakeryProduct
+	{
+		Bread,
+		Cake,
+		Donut
+	}
+	#endregion
+
+	#region Good Code
+	//class Bakery
+	//{
+	//	private OrderManager _orderManager;
+	//	private Oven _oven;
+
+	//	public void RegisterOrder(Customer customer, BakeryProduct product, int quantity)
+	//	{
+	//		_orderManager.RegisterOrder(customer, product, quantity);
+	//		MailManager.SendEmail(customer, "Your order was registered successfully");
+	//	}
+
+	//	public void Bake(Order order)
+	//	{
+	//		_oven.Bake(order);
+	//		MailManager.SendEmail(order.Customer, "Your food is ready");
+	//	}
+
+	//}
+
+	//public class OrderManager
+	//{
+	//	public void RegisterOrder(Customer customer, BakeryProduct product, int quantity)
+	//	{
+	//		//Some logic for registering the order
+	//	}
+	//}
+	//public class Oven
+	//{
+	//	public void Bake(Order order)
+	//	{
+	//		//Some baking logic
+	//	}
+
+	//}
+	//public class MailManager
+	//{
+	//	public static void SendEmail(Customer customer, string message)
+	//	{
+	//		//Sending an email
+	//	}
+
+	//}
+
+	//public class Order
+	//{
+	//	public Customer Customer { get; set; }
+	//}
+	//public class Customer
+	//{
+
+	//}
+	//public enum BakeryProduct
+	//{
+	//	Bread,
+	//	Cake,
+	//	Donut
+	//}
+	#endregion
+}
